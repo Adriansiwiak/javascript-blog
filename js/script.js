d070137e40fc8488de4791a11d6d7c4b23258c4c
Adrian Siwiak <amsiwiak98@gmail.com>
2019-12-10 13:40:55 +0100

    Add preventDefult ti event titleClickHadler

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@
 });
 */
 const titleClickHandler = function (event) {
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
+  
 
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -17,7 +19,7 @@ const titleClickHandler = function (event) {
   /* [IN PROGRES] add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
-  
+
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .active');
 
