efb48affc58e7b5789115a8d28913f30f45bd608
Adrian Siwiak <amsiwiak98@gmail.com>
2020-01-29 17:50:40 +0100

    GenerateTags

--- js/script.js
+++ js/script.js
@@ -43,39 +43,40 @@
 
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles';
-  
-    function generateTitleLinks() {
-    
-      /* remove contents of titleList */
+    optTitleListSelector = '.titles',
+    optArticleTagsSelector = '.post-tags .list';
+
+  function generateTitleLinks() {
+
+    /* remove contents of titleList */
     const titleList = document.querySelector(optTitleListSelector);
     console.log('titleList', titleList);
     titleList.innerHTML = '';
-    
+
     /* for each article */
-    
+
     const articles = document.querySelectorAll('.post');
     console.log('articles', articles);
     let html = '';
     for (let article of articles) {
-      
+
       /* get the article id */
-      
+
       const articleId = article.getAttribute('id');
       console.log('articleId', articleId);
-      
+
       /* find the title element */
-      
+
       const articleTitle = article.querySelector(optTitleSelector).innerHTML;
       console.log('articleTitle', articleId);
-      
+
       /* create HTML of the link */
-      
+
       const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
       console.log('linkHTML', linkHTML);
-      
+
       /* insert link into titleList */
-      
+
       /* titleList.insertAdjacentHTML('beforeend', linkHTML);
         console.log(titleList);*/
       html = html + linkHTML;
@@ -99,26 +100,50 @@
 
     /* START LOOP: for every article: */
 
-    /* find tags wrapper */
+    for (let article of articles) {
 
-    /* make html variable with empty string */
+      /* find tags wrapper */
 
-    /* get tags from data-tags attribute */
+      const taglist = article.querySelectorAll(optArticleTagsSelector);
+      console.log('taglist', taglist);
 
-    /* split tags into array */
+      /* make html variable with empty string */
 
-    /* START LOOP: for each tag */
+      let html = '';
 
-    /* generate HTML of the link */
+      /* get tags from data-tags attribute */
 
-    /* add generated code to html variable */
+      const articleTags = article.getAttribute('data-tags');
+      console.log('articleTags:', articleTags);
 
-    /* END LOOP: for each tag */
+      /* split tags into array */
 
-    /* insert HTML of all the links into the tags wrapper */
+      const articleTagsArray = articleTags.split(' ');
+      console.log('articleTagsArray', articleTagsArray);
 
-    /* END LOOP: for every article: */
+      /* START LOOP: for each tag */
+
+      for(let tag of  articleTagsArray){
+        console.log('tag:', tag);
+
+        /* generate HTML of the link */
+
+        const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+        console.log('linkHTML', linkHTML);
+
+        /* add generated code to html variable */
+
+        html = html + linkHTML;
+
+        /* END LOOP: for each tag */
+      
+      }
+
+      /* insert HTML of all the links into the tags wrapper */
+
+      /* END LOOP: for every article: */
+    }
   }
-  generateTags();
 
+  generateTags();
 }
